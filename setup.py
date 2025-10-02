cd014b1d9f76a71a884d88efde715a102830fb93
Ning Wang <nw8333@princeton.edu>
2025-10-02 04:48:51 -0400

    What: Refactor time_code context manager to its own module Why: Utilities should be organized in their own directory How: Create timer.py and import time_code to other files Testing: Works on test PyPI installation

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open('README.md', mode='r') as readme:
 
 setup(
     name='PIANO',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
